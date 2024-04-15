e4557744e0db4f426978dd7d8a1f9f8b1f7eec60
Joao-Soares1498 <fsoares1498@gmail.com>
2024-04-15 15:05:48 +0100

    local data provided

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,8 @@ import { NgIf, NgFor, UpperCasePipe } from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import {MessageService} from '../message.service'
 
 
 @Component({
@@ -12,10 +13,20 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
- heroes = HEROES;
- selectedHero?: Hero;
 
- onSelected(hero: Hero): void {
-   this.selectedHero = hero;
+ heroes: Hero[] = [];
+
+ constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void{
+    this.getHeroes();
+    }
+
+ getHeroes(): void{
+   this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
    }
+
+
 }
+
+
