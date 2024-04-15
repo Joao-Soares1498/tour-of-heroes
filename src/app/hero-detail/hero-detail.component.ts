694cf1639e2e120aabd72c57aa7eb990729a87b6
Joao-Soares1498 <fsoares1498@gmail.com>
2024-04-15 15:54:16 +0100

    data provided by server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -34,5 +34,13 @@ export class HeroDetailComponent {
     this.location.back();
   }
 
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+}
+
+
 
 }
